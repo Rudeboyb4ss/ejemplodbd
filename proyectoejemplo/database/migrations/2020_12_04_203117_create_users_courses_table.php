c83f74c1132bb2469c053a6b9d5436aefebc95b2
Jorge sandoval <jorge.sandoval.m@usach.cl>
2020-12-04 18:02:16 -0300

    Migraciones

---
+++ proyectoejemplo/database/migrations/2020_12_04_203117_create_users_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_courses', function (Blueprint $table) {
+            $table->id();
+            //foranea
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
+
+            $table->unsignedBigInteger('id_courses');
+            $table->foreign('id_courses')->references('id')->on('courses');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_courses');
+    }
+}
